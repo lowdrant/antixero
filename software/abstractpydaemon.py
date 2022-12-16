9c4e7206b9426ceae7f4232c0d1dd123e6d7d57b
Marion Anderson <marand@umich.edu>
2022-12-16 13:00:19 -0500

    created abstract daemon class & antixero daemon boilerplate

---
+++ software/abstractpydaemon.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+from configparser import ConfigParser
+from copy import deepcopy
+from logging import (CRITICAL, DEBUG, ERROR, INFO, WARNING, Formatter, Logger,
+                     StreamHandler)
+from socket import AF_INET, SOCK_STREAM
+from socket import error as sock_error
+from socket import socket
+from threading import Thread
+
+__all__ = ['readcfg', 'MyDaemon']
+
+def readcfg(fn):
+    conf = ConfigParser()
+    try:
+        conf.read(fn)
+    except OSError as e:
+        print('OSError reading config file')
+    return conf
+
+
+class AbstractPyDaemon():
+    DEFAULT_CONFIG_FN = None
+
+    def __init__(self, fn=None, section='DEFAULT'):
+        # Read Config File
+        if fn is None:
+            fn = self.DEFAULT_CONFIG_FN
+        self.fnconf = fn
+        self.section = section
+        self._update_from_config()
+
+        # Thread Setup
+        self.go = False
+        self.threads = []
+        self.mainthread = Thread(target=self._main, args=())
+        self.mainthread.daemon = True
+        self.sockthread = Thread(target=self._listen, args=())
+        self.sockthread.daemon = True
+        self.threads += [self.mainthread, self.sockthread]
+
+    def _update_from_config(self):
+        conf = readcfg(self.fnconf)
+
+        # Logger Setup
+        self.loglevel = eval(conf[section]['loglevel'])
+        self._configlogger()
+
+        # Socket Setup
+        self.host = conf[section]['host']
+        self.port = int(conf[section]['port'])
+        self.sock = socket(AF_INET, SOCK_STREAM)
+
+    def _configlogger(self):
+        """Update `self.logger` with current loglevel param."""
+        self.logger = Logger(__file__)
+        handler = StreamHandler(stdout)
+        handler.setLevel(self.loglevel)
+        formatter = Formatter('%(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def start(self):
+        """Start daemon threads"""
+        if self.go:
+            self.logger.error('start called but go already True')
+            return
+        try:
+            self.sock.bind((self.host, self.port))
+        except sock_error as e:
+            print(e)
+            self.logger.error(f'start called but {self.host}:{self.port} already in use')
+            return
+        self.go = True
+        self.mainthread.start()
+        self.sockthread.start()
+
+    def stop(self, join=False):
+        """Stop mainloop thread by sending stop signal to socket.
+
+        Parameters
+        ----------
+        join : bool, optional
+            Whether or not to join instance's run threads, by default False
+        """
+        with socket(AF_INET, SOCK_STREAM) as sock:
+            sock.connect( (self.host, self.port) )
+            sock.sendall( b'off' )
+        assert not self.go
+        if join:
+            self.join()
+
+        def join(self):
+        if self.go:
+            self.logger.error('join called but go is True')
+            return False
+        for t in self.threads:
+            try:
+                t.join()
+            except Exception as e:
+                self.logger.error(f'{e}')
+        self.logger.debug('All threads joined')
+
+    def _listen(self):
+        """Shutoff command listener thread. Controlled by `self.go` attribute."""
+        while self.go:
+            try:
+                self.sock.listen()
+                conn, addr = self.sock.accept()
+                data = conn.recv(128).decode('utf-8')
+                # TODO: log received connections
+                if data == 'off':
+                    self.go = False
+                    self.sock.close()
+                    self.logger.info('Shutoff signal recieved. Shutting down...')
+                else:
+                    self.logger.warning(f'Socket connection sent {data} rather than off')
+                    self.fault.value = 1  # turn on fault led to alert user
+                self.logger.debug(
+                    f'socket connection; conn:{conn} addr:{addr} data:{data} go:{self.go}'
+                )
+            except Exception as e:
+                self._errlog(e)
+        self.logger.debug(f'listen thread exited. go:{self.go}')
+
+    def _main(self):
+        """Abstract mainloop method"""
+        raise NotImplementedError('User must override _main method')
+
+if __name__ == '__main__':
+    print('This file contains abstract superclasses')
--- software/antixero.py
+++ software/antixero.py
@@ -3,17 +3,45 @@
 Robotic humidifier control interface + mainloop.
 """
 from argparse import ArgumentParser
-from configparser import ConfigParser
 from logging import (CRITICAL, DEBUG, ERROR, INFO, WARNING, Formatter, Logger,
                      StreamHandler)
-from os import environ
-from pathlib import Path
-from socket import AF_INET, SOCK_STREAM
-from socket import error as sock_error
-from socket import socket
 from sys import stdout
 from threading import Thread
 from time import sleep, time
+
+from abstractpydaemon import AbstractPyDaemon
+
+__all__ = ['AntiXero']
+
+def _servowrite():
+    raise NotImplementedError
+
+class AntiXero(AbstractPyDaemon):
+    DEFAULT_CONFIG_FN = Path(__file__).resolve().parent / 'antixero.ini'
+
+    def __init__(self, fn=None, section='DEFAULT'):
+       self.super().__init__(fn, section)
+       self._update_from_config()  # TODO: is this necessary?
+       self.configthread = Thread(target=self._check4updates, args=())
+       self.configthread.daemon = True
+       self.threads.append(self.configthread)
+
+    def _update_from_config(self):
+        # TODO: override the overrides when this is called later?
+        self.super()._update_from_config()
+        self.t_configcheck = float(conf[self.section]['t_configcheck'])
+        self.servopin = int(conf[self.section]['servopin'])
+
+    def _main(self):
+        while self.go:
+            self.logger.debug('mainloop')
+
+    def _check4updates(self)
+        while self.go:
+            self._update_from_config()
+            sleep(self.t_updatecheck)
+            self.logger.debug('checked for updates')
+
 
 if __name__ == '__main__':
     raise NotImplementedError('sorry!')
